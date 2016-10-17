25e6e8a86517a00015c53b02f16bb2ff2484968b
Dominik Heeb <dominik.heeb@zuehlke.com>
2016-10-17 16:10:02 +0200

    Init commit with WebApi

---
+++ backend/RestApi/RestApi/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ServiceLayer.Services;
+
+namespace RestApi.Controllers
+{
+    [RoutePrefix("api/user")]
+    public class UserController : ApiController
+    {
+        private readonly UserService _userService;
+
+        public UserController()
+        {
+            _userService = new UserService(); //TODO: Dependency Injection
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult GetUsers()
+        {
+            return Ok(_userService.GetAllUser());
+        }
+    }
+}
---
+++ backend/RestApi/ServiceLayer/Services/UserService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using ServiceLayer.Dto;
+
+namespace ServiceLayer.Services
+{
+    public class UserService
+    {
+        public ICollection<UserDto> GetAllUser()
+        {
+            return new List<UserDto> {new UserDto {Id = 1, Name= "Test" }, new UserDto { Id = 1, Name = "Test2" } };
+        }
+    }
+}
